5abba0cfdb11fb51fc4512e85f51b3227c603b0d
Dinesh1899 <dinesh@LAPTOP-IBGKF29A.>
2025-05-11 13:24:24 +0000

    added some template functions in router.py

--- sol/router.py
+++ sol/router.py
@@ -1,3 +1,16 @@
+import LEFDEFParser
+import rtree
+
 ## Detail Router
-def detailed_route(idef, ilef, guide, odef):
-    return None
+def detailed_route(idef, ilef, guide, odef):   
+  return None
+
+
+if __name__ == "__main__":
+  # Example usage
+  idef = "../def/c17.def"
+  ilef = "../lef/c17.lef"
+  guide = "../guide/c17.guide"
+  odef = "../def/c17_out.def"    
+  
+  detailed_route(idef, ilef, guide, odef)
