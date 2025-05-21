be60cb233630b8ab345a7d32e1f2a87991bf84df
Dinesh1899 <dinesh@LAPTOP-IBGKF29A.>
2025-05-21 09:29:00 +0000

    verifed execution of the scripts

--- check.sh
+++ check.sh
@@ -3,25 +3,25 @@ python3 sol/checker.py -i def/c17.def -o outputs/c17_out.def -l lef/sky130.lef
 echo "DRC Checker for c17_out.def done..........................................."
 sleep 2
 echo "DRC Checker for add5_out.def started........................................"
-python3 checker.py -i def/add5.def -o outputs/add5_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/add5.def -o outputs/add5_out.def -l lef/sky130.lef
 echo "DRC Checker for add5_out.def done..........................................."
 sleep 2
 echo "DRC Checker for c432_out.def started........................................"
-python3 checker.py -i def/c432.def -o outputs/c432_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/c432.def -o outputs/c432_out.def -l lef/sky130.lef
 echo "DRC Checker for c432_out.def done..........................................."
 sleep 2
 echo "DRC Checker for c499_out.def started........................................"
-python3 checker.py -i def/c499.def -o outputs/c499_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/c499.def -o outputs/c499_out.def -l lef/sky130.lef
 echo "DRC Checker for c499_out.def done..........................................."
 sleep 2
 echo "DRC Checker for c6288_out.def started........................................"
-python3 checker.py -i def/c6288.def -o outputs/c6288_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/c6288.def -o outputs/c6288_out.def -l lef/sky130.lef
 echo "DRC Checker for c6288_out.def done..........................................."
 sleep 2
 echo "DRC Checker for c7552_out.def started........................................"
-python3 checker.py -i def/c7552.def -o outputs/c7552_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/c7552.def -o outputs/c7552_out.def -l lef/sky130.lef
 echo "DRC Checker for c7552_out.def done..........................................."
 sleep 2
 echo "DRC Checker for spm_out.def started........................................"
-python3 checker.py -i def/spm.def -o outputs/spm_out.def -l lef/sky130.lef
+python3 sol/checker.py -i def/spm.def -o outputs/spm_out.def -l lef/sky130.lef
 echo "DRC Checker for spm_out.def done..........................................."
--- setup.sh
+++ setup.sh
@@ -5,6 +5,3 @@ source dr_env/bin/activate
 ## Installing required packages
 pip install matplotlib rtree networkx
 pip install --force-reinstall pkgs/LEFDEFParser-0.1-cp312-cp312-linux_x86_64.whl
-
-## Running the Detail Router script
-./run.sh > results.log
